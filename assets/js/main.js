54f45e3ba11f534babe09baaf444ed4c1d4b54c1
empatistri <162521773+empatistri@users.noreply.github.com>
2024-05-22 02:10:31 -0700

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementById("header");
-
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 50) {
-        header.classList.add("warna");
-    } else {
-        header.classList.remove("warna");
-    }
-});
